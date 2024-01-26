6f6201997428686d837826f4c970f2a526d6089e
Aman Bisht <aman.bisht@inventum.net>
2024-01-26 06:16:52 +0530

    comleted rotateArray problem

---
+++ Arrays/6.UnionOfTwoArrays/question.js
@@ -0,0 +1,15 @@
+// Given two arrays a[] and b[] of size n and m respectively.
+// The task is to find the number of elements in the union between these two arrays.
+
+// Example 1:
+
+// Input:
+// 5 3
+// 1 2 3 4 5
+// 1 2 3
+// Output:
+// 5
+// Explanation:
+// 1, 2, 3, 4 and 5 are the
+// elements which comes in the union set
+// of both arrays. So count is 5.
