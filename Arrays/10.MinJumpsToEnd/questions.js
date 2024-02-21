cc2c481e2a94e2ecbee3b026873554dcd018ad91
Aman Bisht <aman.bisht@inventum.net>
2024-02-21 06:33:34 +0530

    added dsa sheet

---
+++ Arrays/10.MinJumpsToEnd/questions.js
@@ -0,0 +1,18 @@
+// Input:
+// N = 11
+// arr[] = {1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9}
+// Output: 3
+// Explanation:
+// First jump from 1st element to 2nd
+// element with value 3. Now, from here
+// we jump to 5th element with value 9,
+// and from here we will jump to the last.
+
+// Example 2:
+// Input :
+// N = 6
+// arr = {1, 4, 3, 2, 6, 7}
+// Output: 2
+// Explanation:
+// First we jump from the 1st to 2nd element
+// and then jump to the last element.
